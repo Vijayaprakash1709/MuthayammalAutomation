98a38115291ac3b3e2337d064c1db73bcfd55daf
Vijayaprakash M <90378124+Vijayaprakash1709@users.noreply.github.com>
2023-10-13 13:18:32 +0000

    started

--- October 12th.sql
+++
@@ -1,8 +0,0 @@
-use mec_automation;
-
--- create table data_faculties_new as select * from data_faculties;
--- alter table data_faculties_new add column email varchar(255) not null, add column password varchar(255) not null;
--- describe data_faculties_new;
-
--- alter table data_faculties rename faculties;
--- alter table data_faculties_new rename data_faculties;
--- hod.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-
-const hodRouter=express.Router()
-
-hodRouter.get('',async(req,res)=>{
-    console.log("Welcome HOD to the dashboard")
-})
-
-module.exports=hodRouter
